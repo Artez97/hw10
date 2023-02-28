fc7ee6d98f4c931f17a43352af0d6978a93087bb
Artem <ermilintemka@gmail.com>
2023-02-28 19:15:20 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,44 @@
+const dataInfo = `[
+ {
+    "id": 190863,
+    "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ2BeAzEK_nGIOML8mpEuKRyDTHEyqR6pcWMas2my8qg&s",
+    "answer": "Pennsylvania",
+    "question": "Franklin, Allegheny, Lackawanna",
+    "value": 400,
+    "airdate": "2017-12-26T20:00:00.000Z",
+    "created_at": "2022-12-30T21:32:45.775Z",
+    "updated_at": "2022-12-30T21:32:45.775Z",
+    "category_id": 7349,
+    "game_id": 5866,
+    "invalid_count": null,
+    "category": {
+      "id": 7349,
+      "title": "states by counties",
+      "created_at": "2022-12-30T19:11:05.507Z",
+      "updated_at": "2022-12-30T19:11:05.507Z",
+      "clues_count": 40
+    }
+  },
+  {
+    "id": 119248,
+    "img_url": "https://media.wired.com/photos/5a5bf2988d9e513666f16cc2/master/w_1600%2Cc_limit/dormouse_1000.jpg",
+    "answer": "the Dormouse",
+    "question": "Before Alice joins them, the 3 sitting at the table for the tea party:Mad Hatter, March Hare & _____",
+    "value": 800,
+    "airdate": "2008-07-07T19:00:00.000Z",
+    "created_at": "2022-12-30T19:53:21.598Z",
+    "updated_at": "2022-12-30T19:53:21.598Z",
+    "category_id": 13250,
+    "game_id": 2544,
+    "invalid_count": null,
+    "category": {
+      "id": 13250,
+      "title": "what's missing?",
+      "created_at": "2022-12-30T19:53:20.353Z",
+      "updated_at": "2022-12-30T19:53:20.353Z",
+      "clues_count": 5
+    }
+  }
+
+]
+`;
